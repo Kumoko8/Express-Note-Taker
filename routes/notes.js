8c5406110b472ec63be1facaf3c8435ecb2aacd5
Kumori <tauen8@gmail.com>
2023-08-11 17:43:30 -0500

    completed delete note functionalitygit add -A

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const notesRouter = express.Router()
 const fs = require("fs");
 const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const readFromFile = util.promisify(fs.readFile);
@@ -28,7 +29,7 @@ notesRouter.post('/api/notes', (req, res) =>{
 
     if (req.body) {
         newNote ={
-            id: savedNotes.length + 1,
+            id: uuidv4(),
             title,
             text,
         };
@@ -43,4 +44,31 @@ notesRouter.post('/api/notes', (req, res) =>{
     
 }
 });
+
+notesRouter.delete('/api/notes/:id', async (req, res) =>{
+    console.info(`${req.method} request was received to delete note`)
+    try{
+        const id = req.params.id
+
+        const notesData = await fs.readFileSync('./db/db.json', 'utf-8');
+        const notes = JSON.parse(notesData);
+        
+
+        const index = notes.findIndex(notesData => notesData.id === id);
+        console.log(index);
+        if (index !== -1) {
+             notes.splice(index, 1);
+
+             await fs.writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
+
+            res.status(204).send(); // No content
+         } else {
+            res.status(404).json({ message: 'Note not found' });
+    }
+
+    } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+    }
+})
 module.exports = notesRouter;

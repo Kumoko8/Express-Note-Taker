9274a61525cefb74ac6a0448ac214be01ec98f47
Kumori <tauen8@gmail.com>
2023-10-04 14:19:54 -0500

    debugged server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const fs = require("fs")
 const path = require("path")
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(require(path.join(__dirname, "routes/notes.js")))
@@ -18,16 +17,8 @@ app.get("/notes", (req, res) =>{
     res.sendFile(path.join(__dirname, "public/notes.html"))
 })
 
-//how is this post request connected to the database?
 
 
 app.listen(PORT, () => {
     console.log(`application successful listening to http://localhost:${PORT}`)
 })
-
-// I want the app to be able to take in 
-//notes typed into the program
-//save them in the area on the left
-//and then be able to delete or view them after
-
-//I need to create a route that responds to a POST request 

1258374e93e3db0d1fb3f04289da78e1d36bb4aa
Kumori <tauen8@gmail.com>
2023-10-04 14:41:04 -0500

    edited server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(require(path.join(__dirname, "routes/notes.js")))
 
 app.get('/', (req, res) => {
